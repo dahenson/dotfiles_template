c78835d8524d8b1863075cdc6218c59bfef976a3
Dane Henson <thegreatdane@gmail.com>
2022-08-24 20:45:11 -0500

    Add my new lua init

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,52 @@
+require('user.plugins')
+require('user.language-server')
+
+-- Appearance
+vim.opt.syntax = 'on'
+vim.opt.termguicolors = true
+vim.cmd('colorscheme onedark')
+
+-- General
+vim.opt.colorcolumn = '120'
+vim.opt.backspace = 'indent,eol,start'
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.list = true
+vim.opt.listchars = { tab = '→ ', trail = '•' }
+vim.opt.mouse = 'a'
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 8
+vim.opt.showmatch = false
+vim.opt.smartcase = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.visualbell = true
+
+-- Indentation
+vim.opt.autoindent = true
+vim.opt.breakindent = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+
+-- Keybindings
+
+vim.g.mapleader = ','
+
+vim.keymap.set('n', '0', '^')
+vim.keymap.set('n', '^', '0')
+
+-- Telescope
+vim.keymap.set('n', '<leader>t', '<cmd>Telescope find_files<CR>')
+vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<CR>')
+vim.keymap.set('n', '<leader>lg', '<cmd>Telescope live_grep<CR>')
+vim.keymap.set('n', '<leader>gs', '<cmd>Telescope grep_string<CR>')
+
+-- NERDTree
+vim.keymap.set('n', '<leader>n', '<cmd>NERDTreeFocus<CR>')
+vim.keymap.set('n', '<C-n>', '<cmd>NERDTreeToggle<CR>')
+vim.keymap.set('n', '<C-f>', '<cmd>NERDTreeFind<CR>')
+
---
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -0,0 +1,84 @@
+-- Install Packer
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+local is_bootstrap = false
+
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  is_bootstrap = true
+  local packer_url = 'https://github.com/wbthomason/packer.nvim'
+  vim.fn.system({'git', 'clone', '--depth', '1', packer_url, install_path})
+  vim.cmd('packadd packer.nvim')
+end
+
+-- Install plugins by invoking packer
+require('packer').startup(function(use)
+  -- package manager
+  use 'wbthomason/packer.nvim'
+
+  -- colorscheme
+  use 'navarasu/onedark.nvim'
+
+  -- editor navigation and file management
+  use 'christoomey/vim-tmux-navigator' -- Navigate between tmux panes
+  use 'preservim/nerdtree'             -- File navigation sidebar
+  use 'tpope/vim-fugitive'             -- Git stuff
+
+  -- fuzzy finder
+  use 'BurntSushi/ripgrep'
+  use 'sharkdp/fd'
+  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+  use 'nvim-lua/plenary.nvim'
+  use 'nvim-telescope/telescope.nvim'
+
+  -- language stuff
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'sheerun/vim-polyglot'           -- Language syntax highlighting
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+
+  if is_bootstrap then
+    require('packer').sync()
+  end
+end)
+
+-- When we are bootstrapping a configuration, it doesn't
+-- make sense to execute the rest of the init.lua.
+--
+-- You'll need to restart nvim, and then it will work.
+if is_bootstrap then
+  print '=================================='
+  print '    Plugins are being installed'
+  print '    Wait until Packer completes,'
+  print '       then restart nvim'
+  print '=================================='
+  return
+end
+
+-- Automatically reload and install plugins when init.lua is saved
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
+-- You dont need to set any of these options. These are the default ones. Only
+-- the loading is important
+require('telescope').setup {
+  extensions = {
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    }
+  }
+}
+
+-- To get fzf loaded and working with telescope, you need to call
+-- load_extension, somewhere after setup function:
+require('telescope').load_extension('fzf')
